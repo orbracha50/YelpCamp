d9271b02dbe157fc7b8c749b2d47683fed4a4364
Or Bracha <orbracha50@gmail.com>
2025-12-14 13:06:23 +0200

    edit page

--- app.js
+++ app.js
@@ -29,6 +29,16 @@ app.get('/campgrounds/:id', async (req, res,) => {
     res.render('campgrounds/details', { campground });
 }); 
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
 
 app.listen(3000, () => {
     console.log('app work on port 3000')
